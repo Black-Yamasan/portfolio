549782b173d6f5231eca648d8406bdb6f736715e
Yama <yamawaki00@gmail.com>
2020-04-14 20:46:39 +0900

    modify: import links component

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-import riot from 'riot';
+import * as riot from 'riot'
 import '../scss/_styles.scss';
-import Links from '$Riot/components/links.riot';
-console.log('?')
+import linksComponent from '$Riot/components/linksComponent.riot';
+
+riot.register('links-component', linksComponent);
+riot.mount('links-component');
