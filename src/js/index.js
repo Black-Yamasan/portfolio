9a84e153df0e79a8d3c99d3c47241ced388e79c8
Yama <yamawaki00@gmail.com>
2020-04-16 20:12:32 +0900

    modify: mv animation

--- src/js/index.js
+++ src/js/index.js
@@ -12,83 +12,73 @@ showChangeTheme();
 showAbout();
 showNews();
 showLinks();
-
 scrollMvControll();
 
 
 const canvas = document.getElementById('js-c-mv__canvas');
-let isResize = false;
 
 const app = new PIXI.Application({
-  antialias: true
+  antialias: true,
+  view: canvas
 });
 
-// const createGradTexture = () => {
-//   const quality = 256*4;
-//   const ctx = canvas.getContext('2d');
-//   canvas.width = quality;
-//   canvas.height = 1;
+const starImage = PIXI.Texture.from('/assets/images/star.png');
+const starAmount = 300;
+let cameraZ = 0;
+const fov = 10;
+const baseSpeed = 0.05;
+let speed = 0;
+let warpSpeed = 0;
+const starStretch = 0.8;
+const starBaseSize = 0.02;
 
-//   const grd = ctx.createLinearGradient(0, 0, quality, 0);
-//   grd.addColorStop(0, 'hsla(200, 50%, 50%, 0.2)');
-//   grd.addColorStop(1, 'hsla(280, 50%, 50%, 0.2)');
-//   ctx.fillStyle = grd;
-//   ctx.fillRect(0, 0, quality, 1);
+const randomizeStar = (star, initial) => {
+  star.z = initial ? Math.random() * 1000 : cameraZ + Math.random() * 100 + 1000;
 
-//   return PIXI.Texture.from(canvas);
-// }
+  const deg = Math.random() * Math.PI * 2;
+  const distance = Math.random() * 50 + 1;
+  star.x = Math.cos(deg) * distance;
+  star.y = Math.sin(deg) * distance;
+}
 
-// const gradTexture = createGradTexture();
-// const sprite = new PIXI.Sprite(gradTexture);
+const stars = [];
+for ( let i = 0; i < starAmount; i++ ) {
+  const star = {
+    sprite: new PIXI.Sprite(starImage),
+    z: 0,
+    x: 0,
+    y: 0,
+  };
 
-// sprite.position.set(50, 100);
-// sprite.rotation = 45;
-// sprite.width = 400;
-// sprite.height = 1000;
-// app.state.addChild(sprite);
+  star.sprite.anchor.x = 0.5;
+  star.sprite.anchor.y = 0.5;
+  randomizeStar(star, true);
+  app.stage.addChild(star.sprite);
+  stars.push(star);
+}
 
-// const catImgSize = {
-//   width: 351,
-//   height: 258
-// };
-// let isResize = false;
+setInterval(() => {
+  warpSpeed = warpSpeed > 0 ? 0 : 0.2;
+}, 5000)
 
-// const app = new PIXI.Application({
-//   width: window.innerWidth,
-//   height: window.innerHeight,
-//   autoResize: true,
-//   resolution: devicePixelRatio,
-//   view: screen,
-//   backgroundColor: '0xffffff'
-// });
+app.ticker.add((delta) => {
+  speed += (warpSpeed - speed) / 50;
+  cameraZ += delta * 5 * (speed + baseSpeed);
+  for ( let i = 0; i < starAmount; i++ ) {
+    const star = stars[i];
+    if ( star.z < cameraZ ) randomizeStar(star);
 
-// const container = new PIXI.Container();
-// app.stage.addChild(container);
+    const z = star.z - cameraZ;
+    star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
+    star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
 
-// const catImg = new PIXI.Sprite.from('assets/images/img_cat.png');
-// catImg.width = (500 / 600) * catImgSize.width;
-// catImg.height = (500 / 600) * catImgSize.height;
-// catImg.x = (window.innerWidth / 2) - (catImg.width / 2);
-// catImg.y = window.innerHeight / 2 - (catImg.height / 2);
-// container.addChild(catImg);
+    const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
+    const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
+    const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
+    const distanceScale = Math.max(0, (600 - z) / 800);
+    star.sprite.scale.x = distanceScale * starBaseSize;
 
-// const alphaFilter = new PIXI.filters.AlphaFilter(1);
-// const blurFilter = new PIXI.filters.BlurFilter(270, 1, 10, 0);
-// container.filters = [alphaFilter, blurFilter];
-
-
-// const onResize = () => {
-//   if ( !isResize ) {
-//     window.requestAnimationFrame(() => {
-//       app.renderer.resize(window.innerWidth, window.innerHeight);
-//       catImg.width = (500 / 600) * catImgSize.width;
-//       catImg.height = (500 / 600) * catImgSize.height;
-//       catImg.x = (window.innerWidth / 2) - (catImg.width / 2);
-//       catImg.y = window.innerHeight / 2 - (catImg.height / 2);
-//       isResize = false;
-//     });
-//     isResize = true;
-//   }
-// };
-
-// window.addEventListener('resize', onResize, true);
+    star.sprite.scale.y = distanceScale * starBaseSize + distanceScale * speed * starStretch * distanceCenter / app.renderer.screen.width;
+    star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+  }
+});
